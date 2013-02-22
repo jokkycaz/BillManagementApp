fb708d962a47769ad1a6a489b2015e33cb4f0562
jokkycaz <jokkycaz@gmail.com>
2013-02-21 20:23:36 -0500

    SlidingDrawer added for editing bills Validation for creating bill

---
+++ BillManagement/src/com/example/billmanagement/inputValidation.java
@@ -0,0 +1,35 @@
+package com.example.billmanagement;
+
+public class inputValidation {
+	@SuppressWarnings("deprecation")
+	public static Boolean isName(String s){
+		s = s.replaceAll("\\s", "");
+		for(int i = 0; i < s.length(); i++){
+			if(!Character.isLetter(s.charAt(i))){
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	public static Boolean isCurrency(String s){
+		for(int i = 0; i < s.length(); i++){
+			if(!Character.isDigit(s.charAt(i)) && s.charAt(i)!='.' && s.charAt(i)!='$'){
+				System.out.println(s.charAt(i));
+				return false;
+			}
+			
+		}
+		return true;
+	}
+	public static Boolean isDate(String s){
+		if(!Character.isDigit(s.charAt(0)) && !Character.isDigit(s.charAt(1)) && s.charAt(2)!='-' &&
+				!Character.isDigit(s.charAt(3)) && !Character.isDigit(s.charAt(4)) && s.charAt(5)!='-' &&
+				!Character.isDigit(s.charAt(6)) && !Character.isDigit(s.charAt(7)) && 
+				!Character.isDigit(s.charAt(8)) &&!Character.isDigit(s.charAt(9))){
+			return false;
+		}
+		return true;
+	}
+
+}
